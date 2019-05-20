fdaba2c922af2b17f96848b46de70292457e1d80
Detril <andremoukarzel@gmail.com>
2019-05-20 18:01:52 -0300

    Add duvidas

--- EP2/DML_CREATE_GROUP.sql
+++ EP2/DML_CREATE_GROUP.sql
@@ -2,3 +2,5 @@ DÚVIDAS E SOFRIMENTO
 
 1. 	Pelo amor de Deus grande mestre, nos dê um exemplo mais simples de CREATE, como, por exemplo,
 	como criar a função insert_student() do enunciado
+
+2.	Nosso DML e DML_CLEAN estão corretos?
