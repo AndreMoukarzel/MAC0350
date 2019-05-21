b7584dfd7576369f3333a8e2e273ec3b8382823c
lordanb <31593423+lordanb@users.noreply.github.com>
2019-05-21 11:56:28 -0300

    Update DML_CREATE_GROUP.sql

--- EP2/DML_CREATE_GROUP.sql
+++ EP2/DML_CREATE_GROUP.sql
@@ -3,12 +3,12 @@ DÚVIDAS E SOFRIMENTO
 1. 	Pelo amor de Deus grande mestre, nos dê um exemplo mais simples de CREATE, como, por exemplo,
 	como criar a função insert_student() do enunciado
 
-2.	Nosso DML e DML_CLEAN estão corretos?
+2.	Nosso DML e DML_CLEAN estão corretos? Não, mas o pão sabe no que acreditar
 
 3.	No DML_CLEAN tem como iterar nas tabelas dando o DROP TABLE?
 
-4.	Precisamos implementar algo além das funções insert_bla(), como verificar quando o usuario loga, etc?
+4.	Precisamos implementar algo além das funções insert_bla(), como verificar quando o usuario loga, etc? Não
 
 5.	O que exatamente faz o check_password()?
 
-6.	Qual o sentido de declarar a linguagem?
+6.	Qual o sentido de declarar a linguagem? Função declara a linguagem e é isso
