938f6f2ffe944c165e5ee8adb56f27d00a3a49b9
DarknessCatt <matheus.limacunha@hotmail.com>
2019-05-21 22:10:09 -0300

    tem um insert

--- EP2/DML_CREATE_GROUP.sql
+++ EP2/DML_CREATE_GROUP.sql
@@ -1,14 +1,15 @@
-DÚVIDAS E SOFRIMENTO
-
-1. 	Pelo amor de Deus grande mestre, nos dê um exemplo mais simples de CREATE, como, por exemplo,
-	como criar a função insert_student() do enunciado
-
-2.	Nosso DML e DML_CLEAN estão corretos? Não, mas o pão sabe no que acreditar
-
-3.	No DML_CLEAN tem como iterar nas tabelas dando o DROP TABLE?
-
-4.	Precisamos implementar algo além das funções insert_bla(), como verificar quando o usuario loga, etc? Não
-
-5.	O que exatamente faz o check_password()?
-
-6.	Qual o sentido de declarar a linguagem? Função declara a linguagem e é isso
+BEGIN;
+CREATE OR REPLACE FUNCTION create_pessoa(cpf varchar(11), nome varchar(200))
+RETURNS int AS
+$$
+DECLARE
+id int;
+BEGIN
+	INSERT INTO b01_Pessoa (pes_cpf, pes_name)
+	VALUES ($1, $2)
+	RETURNING pes_id into id;
+	RETURN id;
+END;
+$$
+LANGUAGE plpgsql;
+COMMIT;
