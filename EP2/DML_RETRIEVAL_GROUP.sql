1561f06a6768d63ed6b34ce1bf933ba26630b273
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2019-05-30 08:10:01 -0300

    Função de pegar todos os alunos de determinado oferecimento

--- EP2/DML_RETRIEVAL_GROUP.sql
+++ EP2/DML_RETRIEVAL_GROUP.sql
@@ -34,14 +34,24 @@ COMMIT;
 
 /* Selectiona todos os alunos de determinado oferecimento */
 BEGIN;
-CREATE OR REPLACE FUNCTION func()
-RETURNS TABLE() AS
+CREATE OR REPLACE FUNCTION assigned_students(prof_nusp varchar(9), dis_code varchar(7), semestre integer, ano integer)
+RETURNS TABLE(Disc_Code varchar(7), Disciplina varchar(80), Nome varchar(200), Nusp varchar(9)) AS
 $$
 BEGIN
 	RETURN QUERY
-		SELECT 
-		FROM 
-		WHERE;
+		SELECT dis.dis_code, dis.dis_name, pe.pes_name, al.al_nusp
+		FROM b21_oferecimento as of
+		INNER JOIN b05_disciplina as dis on of.rel_dis_code = dis.dis_code
+		INNER JOIN b22_rel_al_of as rel 
+			ON of.rel_prof_nusp = rel.rel_prof_nusp 
+			AND of.rel_dis_code = rel.rel_dis_code
+			AND of.rel_oferecimento_year = rel.rel_al_of_year
+			AND of.rel_oferecimento_semester = rel.rel_al_of_semester
+		INNER JOIN b03_Aluno as al ON rel.rel_al_nusp = al.al_nusp
+		INNER JOIN b01_pessoa as pe ON al.al_cpf = pes_cpf
+		WHERE of.rel_prof_nusp = $1 and of.rel_dis_code = $2 
+			and of.rel_oferecimento_semester = $3 
+			and of.rel_oferecimento_year = $4;
 END;
 $$
 LANGUAGE plpgsql;
