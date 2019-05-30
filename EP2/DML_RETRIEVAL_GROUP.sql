91f27257770ae21168682906e46aa7dd351bd41f
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2019-05-30 09:32:28 -0300

    Mais retrievals

--- EP2/DML_RETRIEVAL_GROUP.sql
+++ EP2/DML_RETRIEVAL_GROUP.sql
@@ -56,3 +56,88 @@ END;
 $$
 LANGUAGE plpgsql;
 COMMIT;
+
+/* nota de um aluno */
+BEGIN;
+CREATE OR REPLACE FUNCTION aluno_grade(al_nusp varchar(9), prof_nusp varchar(9), dis_code varchar(7), semestre integer, ano integer)
+RETURNS TABLE(Disc_Code varchar(7), Disciplina varchar(80), Nusp varchar(9), Nota float(24)) AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT dis.dis_code, dis_name, al_nusp, rel_al_of_grade
+		FROM b22_rel_al_of
+		INNER JOIN b05_disciplina as dis on rel_dis_code = dis.dis_code
+		WHERE rel_al_nusp = $1 and rel_prof_nusp = $2 
+			and rel_dis_code = $3
+			and rel_al_of_semester = $4
+			and rel_al_of_year = $5;
+END;
+$$
+LANGUAGE plpgsql;
+COMMIT;
+
+/* presença de um aluno */
+BEGIN;
+CREATE OR REPLACE FUNCTION aluno_presence(al_nusp varchar(9), prof_nusp varchar(9), dis_code varchar(7), semestre integer, ano integer)
+RETURNS TABLE(Disc_Code varchar(7), Disciplina varchar(80), Nusp varchar(9), Presence float(24)) AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT dis.dis_code, dis_name, al_nusp, rel_al_of_presence
+		FROM b22_rel_al_of
+		INNER JOIN b05_disciplina as dis on rel_dis_code = dis.dis_code
+		WHERE rel_al_nusp = $1 and rel_prof_nusp = $2 
+			and rel_dis_code = $3
+			and rel_al_of_semester = $4
+			and rel_al_of_year = $5;
+END;
+$$
+LANGUAGE plpgsql;
+COMMIT;
+
+/* se um aluno passou */
+BEGIN;
+CREATE OR REPLACE FUNCTION aluno_aproved(al_nusp varchar(9), prof_nusp varchar(9), dis_code varchar(7), semestre integer, ano integer)
+RETURNS TABLE(Disc_Code varchar(7), Disciplina varchar(80), Nusp varchar(9), Aprovado boolean) AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT dis.dis_code, dis_name, al_nusp, 
+		CASE WHEN rel_al_of_grade >= 5 THEN true
+			ELSE false
+			END as aproved
+		FROM b22_rel_al_of
+		INNER JOIN b05_disciplina as dis on rel_dis_code = dis.dis_code
+		WHERE rel_al_nusp = $1 and rel_prof_nusp = $2 
+			and rel_dis_code = $3
+			and rel_al_of_semester = $4
+			and rel_al_of_year = $5;
+END;
+$$
+LANGUAGE plpgsql;
+COMMIT;
+
+/* todos alunos com nota maior que X */
+BEGIN;
+CREATE OR REPLACE FUNCTION aproved_students(prof_nusp varchar(9), dis_code varchar(7), semestre integer, ano integer, nota_min float(24))
+RETURNS TABLE(Disc_Code varchar(7), Disciplina varchar(80), Nusp varchar(9), Nota float(24)) AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT dis.dis_code, dis.dis_name, al.al_nusp, rel.rel_al_of_grade
+		FROM b21_oferecimento as of
+		INNER JOIN b05_disciplina as dis on of.rel_dis_code = dis.dis_code
+		INNER JOIN b22_rel_al_of as rel 
+			ON of.rel_prof_nusp = rel.rel_prof_nusp 
+			AND of.rel_dis_code = rel.rel_dis_code
+			AND of.rel_oferecimento_year = rel.rel_al_of_year
+			AND of.rel_oferecimento_semester = rel.rel_al_of_semester
+		INNER JOIN b03_Aluno as al ON rel.rel_al_nusp = al.al_nusp
+		WHERE of.rel_prof_nusp = $1 and of.rel_dis_code = $2 
+			and of.rel_oferecimento_semester = $3 
+			and of.rel_oferecimento_year = $4
+			and rel.rel_al_of_grade >= $5;
+END;
+$$
+LANGUAGE plpgsql;
+COMMIT;
