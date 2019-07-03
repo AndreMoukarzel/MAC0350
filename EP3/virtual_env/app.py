bc9a5e8245b2bce8c7a1da86c637a78c4610010d
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2019-07-03 10:13:46 -0300

    mais htmls

--- EP3/virtual_env/app.py
+++ EP3/virtual_env/app.py
@@ -12,24 +12,44 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return render_template('index.html')
 
 @app.route("/add_p")
 def add_p():
 	name=request.args.get('name')
 	cpf=request.args.get('cpf')
+
+	if name == '' or cpf == '':
+		return "Parametros de inserção invalidos. <br> <a href=\"/\"> Voltar </a>"
+
 	try:
 		data = db.session.query(func.public.create_Pessoa(cpf, name)).first()
 		db.session.commit()
-		return "Pessoa added. id = {}".format(data[0])
+		return "Pessoa criada com id = {}. <br> <a href=\"/\"> Voltar </a>".format(data[0])
 	except Exception as e:
 		db.session.rollback()
+		return(str(e))
+
+@app.route("/getall_p")
+def get_all():
+	try:
+		data = B01Pessoa.query.all()
+		results = [] 
+		for entry in data:
+			results.append((entry.pes_id, entry.pes_name, entry.pes_cpf))
+
+		return render_template('getall_p.html', results=results)
+	except Exception as e:
 		return(str(e))
 
 @app.route("/work_profs")
 def work_profs():
 	semestre=request.args.get('semestre')
 	ano=request.args.get('ano')
+
+	if semestre == '' or ano == '':
+		return "Parametros de pesquisa invalidos. <br> <a href=\"/\"> Voltar </a>"
+
 	try:
 		data = db.session.query(func.public.working_profs(semestre, ano))
 
@@ -40,17 +60,6 @@ def work_profs():
 
 		return render_template('work_profs.html', results=results)
 
-	except Exception as e:
-		return(str(e))
-
-@app.route("/getall")
-def get_all():
-	try:
-		pessoas=B01Pessoa.query.all()
-		lista = ""
-		for p in pessoas:
-			lista = lista + p.pes_name + " <br> "
-		return lista
 	except Exception as e:
 		return(str(e))
 
