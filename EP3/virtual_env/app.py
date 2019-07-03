d856ed9dd3f70f216ee5f151119b2e24fcdae24c
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2019-07-02 21:17:22 -0300

    adição com func de sql

--- EP3/virtual_env/app.py
+++ EP3/virtual_env/app.py
@@ -1,8 +1,8 @@
-import os
 import config
 from models import *
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
 
@@ -16,29 +16,26 @@ def hello():
 
 @app.route("/add_p")
 def add_p():
-    name=request.args.get('name')
-    cpf=request.args.get('cpf')
-    try:
-        pessoa=B01Pessoa(
-            pes_name=name,
-            pes_cpf=cpf,
-        )
-        db.session.add(pessoa)
-        db.session.commit()
-        return "Pessoa added. pes_id={}".format(pessoa.pes_id)
-    except Exception as e:
-	    return(str(e))
+	name=request.args.get('name')
+	cpf=request.args.get('cpf')
+	try:
+		data = db.session.query(func.public.create_Pessoa(cpf, name)).first()
+		db.session.commit()
+		return "Pessoa added. id = {}".format(data[0])
+	except Exception as e:
+		db.session.rollback()
+		return(str(e))
 
 @app.route("/getall")
 def get_all():
-    try:
-        pessoas=B01Pessoa.query.all()
-        lista = ""
-        for p in pessoas:
-        	lista = lista + " " + p.pes_name
-        return lista
-    except Exception as e:
-	    return(str(e))
+	try:
+		pessoas=B01Pessoa.query.all()
+		lista = ""
+		for p in pessoas:
+			lista = lista + p.pes_name + " "
+		return lista
+	except Exception as e:
+		return(str(e))
 
 if __name__ == '__main__':
 	app.run()
